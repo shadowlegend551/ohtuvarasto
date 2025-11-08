c09844241454e8412964bfa280710c95a1f1ac78
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-08 17:08:27 +0200

    100% test coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_init_with_wrong_tilavuus_and_saldo(self):
+        wrong_tilavuus_and_saldo = Varasto(-1, alku_saldo=-1)
+        self.assertAlmostEqual(wrong_tilavuus_and_saldo.tilavuus, 0)
+        self.assertAlmostEqual(wrong_tilavuus_and_saldo.saldo, 0)
+
+    def test_init_with_too_large_alku_saldo(self):
+        wrong_alku_saldo = Varasto(1, alku_saldo=2)
+        self.assertAlmostEqual(wrong_alku_saldo.saldo, 1)
+
+    def test_add(self):
+        # Of form (value_to_add, resulting_value)
+        values = [(-1, 0), (2, 1)]
+        for added, actual in values:
+            with self.subTest(added=added, actual=actual):
+                varasto = Varasto(1)
+                varasto.lisaa_varastoon(added)
+                self.assertAlmostEqual(varasto.saldo, actual)
+
+    def test_remove(self):
+        values = [(-1, 0), (2, 1)]
+        for expected, actual in values:
+            with self.subTest(expecte=expected, actual=actual):
+                varasto = Varasto(1, alku_saldo=1)
+                self.assertAlmostEqual(varasto.ota_varastosta(expected),
+                                       actual)
+
+    def test_string_dunder(self):
+        varasto = Varasto(1)
+        self.assertEqual(varasto.__str__(), 'saldo = 0, vielä tilaa 1')
